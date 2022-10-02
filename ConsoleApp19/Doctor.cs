7c2355b959a4a86264aa522a68c4b530cdf43e9e
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2022-10-02 17:19:37 +0400

    Add project files.

---
+++ ConsoleApp19/Doctor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Doctor : Human
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public List<string> Worktime = new List<string>();
+        public Doctor(int id, string name, string surname, int year) : base(name, surname)
+        {
+            Id = id;
+            Year = year;
+            Worktime.Add("9:00-11:00");
+            Worktime.Add("12:00-14:00");
+            Worktime.Add("15:00-17:00");
+        }
+
+        public override string ToString()
+        {
+            return @$"Id: {Id}
+Year: {Year}";
+        }
+    }
+}
